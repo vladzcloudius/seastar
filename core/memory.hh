4da5fe04f152a38d4a100465c75bf23a70fa04be
Vlad Zolotarov <vladz@scylladb.com>
2017-06-28 19:44:08 -0400

    core::memory: use SEASTAR_PAGE_BITS macro
    Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>

--- core/memory.hh
+++ core/memory.hh
@@ -53,10 +53,9 @@ namespace seastar {
 namespace memory {
 
 /// \cond internal
-// TODO: Use getpagesize() in order to learn a size of a system PAGE.
-static constexpr size_t page_bits = 12;
-static constexpr size_t page_size = 1 << page_bits;       // 4K
-static constexpr size_t huge_page_size = 512 * page_size; // 2M
+static constexpr size_t page_bits = SEASTAR_PAGE_BITS;
+static constexpr size_t page_size = 1 << page_bits;
+static constexpr size_t huge_page_size = 512 * page_size; // FIXME: PPC64 uses 16MB huge pages and 64K PAGESIZE, which gives huge_page_size = 256 * page_size
 
 void configure(std::vector<resource::memory> m, bool mbind,
         std::experimental::optional<std::string> hugetlbfs_path = {});
