3b8642397f537bb29c7867d6e7eadeb268846a47
Vlad Zolotarov <vladz@scylladb.com>
2017-06-28 17:47:58 -0400

    configure.py: add --page-bits parameter
    This parameter defines the value of the SEASTAR_PAGE_BITS macro that
    will be used for defining a seastar::memory::page_bits value.
    
    By default uses the system's PAGE_BITS value.
    
    Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>

--- configure.py
+++ configure.py
@@ -16,7 +16,7 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-import os, os.path, textwrap, argparse, sys, shlex, subprocess, tempfile, re
+import os, os.path, textwrap, argparse, sys, shlex, subprocess, tempfile, re, math
 
 configure_args = str.join(' ', [shlex.quote(x) for x in sys.argv[1:]])
 
@@ -196,6 +196,10 @@ def adjust_visibility_flags(compiler):
     else:
         return ''
 
+def get_page_bits():
+    pagesize = float(subprocess.check_output(['getconf', 'PAGESIZE']).strip())
+    return int(math.log(pagesize, 2))
+
 modes = {
     'debug': {
         'sanitize': '-fsanitize=address -fsanitize=leak -fsanitize=undefined',
@@ -310,6 +314,7 @@ arg_parser.add_argument('--static-stdc++', dest = 'staticcxx', action = 'store_t
                         help = 'Link libgcc and libstdc++ statically')
 arg_parser.add_argument('--static-boost', dest = 'staticboost', action = 'store_true',
                         help = 'Link with boost statically')
+arg_parser.add_argument('--page-bits', dest = 'pagebits', help = 'Memory page bits', default=get_page_bits(), type=int)
 add_tristate(arg_parser, name = 'hwloc', dest = 'hwloc', help = 'hwloc support')
 add_tristate(arg_parser, name = 'backtrace', dest = 'libunwind', help = 'backtrace support (provided by libunwind)')
 arg_parser.add_argument('--enable-gcc6-concepts', dest='gcc6_concepts', action='store_true', default=False,
@@ -626,6 +631,7 @@ tests_link_rule = 'link' if args.tests_debuginfo else 'link_stripped'
 sanitize_flags = sanitize_vptr_flag(args.cxx)
 
 visibility_flags = adjust_visibility_flags(args.cxx)
+pagebits = args.pagebits
 
 if not try_compile(args.cxx, '#include <gnutls/gnutls.h>'):
     print('Seastar requires gnutls.  Install gnutls-devel/libgnutls-dev')
@@ -761,7 +767,7 @@ with open(buildfile, 'w') as f:
         full_builddir = {srcdir}/$builddir
         cxx = {cxx}
         # we disable _FORTIFY_SOURCE because it generates false positives with longjmp() (core/thread.cc)
-        cxxflags = -std=gnu++1y {dbgflag} {fpie} -Wall -Werror -Wno-error=deprecated-declarations -fvisibility=hidden {visibility_flags} -pthread -I{srcdir} -U_FORTIFY_SOURCE {user_cflags} {warnings} {defines}
+        cxxflags = -std=gnu++1y {dbgflag} {fpie} -Wall -Werror -Wno-error=deprecated-declarations -fvisibility=hidden {visibility_flags} -DSEASTAR_PAGE_BITS={pagebits} -pthread -I{srcdir}-U_FORTIFY_SOURCE {user_cflags} {warnings} {defines}
         ldflags = {dbgflag} -Wl,--no-as-needed {static} {pie} -fvisibility=hidden {visibility_flags} -pthread {user_ldflags}
         libs = {libs}
         pool link_pool
