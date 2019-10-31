42b93ecfd7c939bb4666eedb1d95841d71456860
Vlad Zolotarov <vladz@scylladb.com>
2019-10-31 13:46:48 -0400

    thread: add a helper class for abort signals handling
    Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>

--- include/seastar/core/thread.hh
+++ include/seastar/core/thread.hh
@@ -29,6 +29,8 @@
 #include <seastar/core/timer.hh>
 #include <seastar/core/reactor.hh>
 #include <seastar/core/scheduling.hh>
+#include <seastar/core/sharded.hh>
+#include <seastar/core/abort_source.hh>
 #include <memory>
 #include <setjmp.h>
 #include <type_traits>
@@ -285,6 +287,46 @@ futurize_t<std::result_of_t<std::decay_t<Func>(std::decay_t<Args>...)>>
 async(Func&& func, Args&&... args) {
     return async(thread_attributes{}, std::forward<Func>(func), std::forward<Args>(args)...);
 }
+
+class stop_signal {
+    bool _caught = false;
+    condition_variable _cond;
+    sharded<abort_source> _abort_sources;
+    future<> _broadcasts_to_abort_sources_done = make_ready_future<>();
+private:
+    void signaled() {
+        if (_caught) {
+            return;
+        }
+        _caught = true;
+        _cond.broadcast();
+        _broadcasts_to_abort_sources_done = _broadcasts_to_abort_sources_done.then([this] {
+            return _abort_sources.invoke_on_all(&abort_source::request_abort);
+        });
+    }
+public:
+    stop_signal() {
+        _abort_sources.start().get();
+        engine().handle_signal(SIGINT, [this] { signaled(); });
+        engine().handle_signal(SIGTERM, [this] { signaled(); });
+    }
+    ~stop_signal() {
+        // There's no way to unregister a handler yet, so register a no-op handler instead.
+        engine().handle_signal(SIGINT, [] {});
+        engine().handle_signal(SIGTERM, [] {});
+        _broadcasts_to_abort_sources_done.get();
+        _abort_sources.stop().get();
+    }
+    future<> wait() {
+        return _cond.wait([this] { return _caught; });
+    }
+    bool stopping() const {
+        return _caught;
+    }
+    abort_source& as_local_abort_source() { return _abort_sources.local(); }
+    sharded<abort_source>& as_sharded_abort_source() { return _abort_sources; }
+};
+
 /// @}
 
 }
