e85661db87ae38bc0acce094448e90e8e2bd0f0f
Vlad Zolotarov <vladz@scylladb.com>
2019-12-27 18:16:51 -0500

    dpdk: IOVA map only data buffers
    Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>

--- src/net/dpdk.cc
+++ src/net/dpdk.cc
@@ -1768,6 +1768,13 @@ void* dpdk_qp<HugetlbfsMemBackend>::alloc_mempool_xmem(
         return nullptr;
     }
 
+    rte_iova_t iova = rte_mem_virt2iova((const void*)xmem);
+
+    if (rte_vfio_dma_map(xmem, iova, xmem_size)) {
+        free(xmem);
+        return nullptr;
+    }
+
     return xmem;
 }
 
@@ -1873,6 +1880,8 @@ bool dpdk_qp<HugetlbfsMemBackend>::init_rx_mbuf_pool()
 template <bool HugetlbfsMemBackend>
 bool dpdk_qp<HugetlbfsMemBackend>::map_dma()
 {
+    return true;
+
     auto m = memory::get_memory_layout();
     rte_iova_t iova = rte_mem_virt2iova((const void*)m.start);
 
